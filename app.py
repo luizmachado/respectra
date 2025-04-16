335512c723cf3b5296eefb7c5bb0c38d2b206c90
luizmachado.to <luizmachado.to@gmail.com>
2025-04-16 20:21:09 +0000

    change llm to use gemini

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from langchain_google_genai import ChatGoogleGenerativeAI
+from chains import json_chain
+from chains import julia_chain
+from chains import general_chain
+from prompts import responde_prompt
+from prompts import routing_prompt
+from langchain_core.runnables import RunnableBranch
+from langchain_core.output_parsers import StrOutputParser
+import logging
+import json
+import os
+
+app = Flask(__name__)
+
+# Configuração da pasta para upload dos arquivos
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def chat_ia(query):
+    # Substitui ChatOllama pelo modelo Gemini
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    route_prompt = routing_prompt()
+    routing_chain = (route_prompt | llm | StrOutputParser())
+
+    # Definindo os branches para diferentes rotas
+    branch = RunnableBranch(
+        (lambda x: "gerarjson" in x["topic"].lower(), json_chain),
+        (lambda x: "execjulia" in x["topic"].lower(), julia_chain),
+        lambda x: general_chain,
+    )
+
+    full_chain = (
+        {"topic": routing_chain, "question": lambda x: x["question"]} | branch
+    )
+
+    resultado = full_chain.invoke({"question": query})
+    return resultado
+
+
+# Lista para armazenar mensagens do chat
+chat_messages = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file_mapping = {
+                'file_cif': 'CIF',
+                'file_MvsH1': 'MvsH1',
+                'file_MvsH2': 'MvsH2',
+                'file_MvsH3': 'MvsH3',
+                'file_MvsT1': 'MvsT1',
+                'file_MvsT2': 'MvsT2',
+                'file_MvsT3': 'MvsT3',
+                }
+
+        for input_name, new_filename in file_mapping.items():
+            file = request.files.get(input_name)
+            if file and file.filename != '':
+                ext = os.path.splitext(file.filename)[1]
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'],
+                                        new_filename + ext)
+                file.save(filepath)
+    return render_template('index.html', chat_messages=chat_messages)
+
+@app.route('/send_message', methods=['POST'])
+
+def send_message():
+    # Receber mensagem do chat via AJAX
+    query_input = None
+    output = None
+    if request.method == 'POST':
+        query_input = request.form.get('message')
+        if query_input:
+            try:
+                print(query_input)
+                output = chat_ia(query_input)
+                print(output)
+            except Exception as e:
+                logging.error(f"Erro ao acessar modelo de linguagem: {e}")
+                output = "Desculpe, houve um erro ao processar a sua requisição."
+    return jsonify({'status': 'success', 'messages': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ chains.py
@@ -0,0 +1,64 @@
+from langchain_core.runnables import chain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import JsonOutputParser
+from prompts import cef_json_prompt, responde_prompt, chart_prompt
+from utils.tools import format_and_save_json, verificar_arquivos, exec_graph, limpa_temp
+from models import AjusteCompleto
+from os import chdir
+from src_julia.exec_julia import ajuste_param
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from prompts import routing_prompt
+from langchain_core.runnables import RunnableBranch
+import os
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
+
+parser = JsonOutputParser(pydantic_object=AjusteCompleto)
+
+arquivos_txt = ['ConfSpin_Calc.txt', 'CsT_calc.txt', 'Entrop_calc.txt', 'MH1_calc.txt', 'MH2_calc.txt', 'MT1_calc.txt', 'MT2_calc.txt']
+julia_dir = './src_julia'
+
+
+def routing_chain():
+    prompt = routing_prompt()
+    return (prompt | llm | StrOutputParser())
+
+# Função para definir qual cadeia de execução será utilizada
+def create_routing_branch(json_chain, julia_chain, general_chain):
+    return RunnableBranch(
+        (lambda x: "gerarjson" in x["topic"].lower(), json_chain),
+        (lambda x: "execjulia" in x["topic"].lower(), julia_chain),
+        lambda x: general_chain,
+    )
+
+@chain
+def general_chain(text):
+    limpa_temp()
+    prompt = responde_prompt()
+    return (prompt | llm | StrOutputParser()).invoke({"question": text})
+
+# Função para gerar JSON a partir do modelo
+@chain
+def json_chain(text):
+    prompt = cef_json_prompt(parser)
+    gerarjson_chain = (prompt | llm | parser)
+    output = gerarjson_chain.invoke({"question": text})
+    return format_and_save_json(str(output))
+
+@chain
+def julia_chain(text):
+    prompt = chart_prompt
+    gerarchart_chain = (prompt | llm | StrOutputParser())
+    output = gerarchart_chain.invoke({"question": text})
+    output = output.rstrip()
+    if verificar_arquivos(arquivos_txt, julia_dir):
+        exec_graph(output)
+        print(output)
+
+    else:
+        chdir(julia_dir)
+        ajuste_param()
+        chdir('../')
+        exec_graph(output)
+        return "Função Julia executada com sucesso"
